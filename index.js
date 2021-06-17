9ed0acb569a1f271df4cc93001b14a165f153ba3
Nicholas Shurtleff <Nicholas.Shurtleff@myccmortgage.com>
2021-06-17 10:27:02 -0400

    Switching over to us open and seeing what happens

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function getLeaders() {
             "method": "GET",
             "hostname": "golf-leaderboard-data.p.rapidapi.com",
             "port": null,
-            "path": "/leaderboard/285",
+            "path": "/leaderboard/289",
             "headers": {
                 "x-rapidapi-key": "21ce5dac67msh86911ecea6ef3cfp13b4f3jsn734afe0aa2df",
                 "x-rapidapi-host": "golf-leaderboard-data.p.rapidapi.com",
