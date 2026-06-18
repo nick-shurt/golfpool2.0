5f70be6881ed22a548e46f258b561ad63f2f5f79
Nick Shurtleff <nshurtleff15@gmail.com>
2026-06-18 10:33:09 -0400

    Updating pool for 2026 US open

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function getLeaders() {
             "method": "GET",
             "hostname": "golf-leaderboard-data.p.rapidapi.com",
             "port": null,
-            "path": "/leaderboard/843",
+            "path": "/leaderboard/848",
             "headers": {
                 "x-rapidapi-key": "21ce5dac67msh86911ecea6ef3cfp13b4f3jsn734afe0aa2df",
                 "x-rapidapi-host": "golf-leaderboard-data.p.rapidapi.com",
